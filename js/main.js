2d715dc41f9e7e7130cf87bf39a21a9ef0d89476
1stfaccia1 <chad.m.wittman75@gmail.com>
2018-12-24 20:25:51 -0600

    add

---
+++ js/main.js
@@ -0,0 +1,9 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+    $('.carousel').carousel({fullwidth:true});
+    $('.parallax').parallax();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
